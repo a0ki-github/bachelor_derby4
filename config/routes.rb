5cebed748486a6eaeb16cf74111bf03d89e71c78
a0ki-github <aoki.hotaka@gmail.com>
2021-11-05 14:41:48 +0900

    Add: admin以外のルーティングをhome#indexに集約

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   namespace :admin do    
     resources :candidates
   end
+  get '*path', to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
