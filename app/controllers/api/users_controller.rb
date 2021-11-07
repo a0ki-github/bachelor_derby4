1a6ffa30d4c4856ecd7ce3893bec84c66e5f6822
a0ki-github <aoki.hotaka@gmail.com>
2021-11-07 16:25:08 +0900

    Add: Api::User#createを実装

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,16 @@
 class Api::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
