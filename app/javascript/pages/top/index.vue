cac53c1d51b407c29d3dd1e37d1b5408acd5695c
a0ki-github <aoki.hotaka@gmail.com>
2021-11-07 15:36:18 +0900

    Fix: 不要なクラスを削除

--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -6,7 +6,7 @@
       li BETするメンバーは変更可能だが、 <br> 番組開始(11/25 22:00)後は1回につき-10P
       li 各放送日時点で脱落メンバーにBETしているユーザー、または誰にもBETしていないユーザーには-20P
       li 番組終了時に得点上位者を発表
-    .link.text-center
+    .text-center
       router-link(
         class="btn btn-dark m-5 router-link-active"
         :to="{ name: 'LoginIndex' }"
