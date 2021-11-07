7e84f6f949657095f703749a5dfd412f90e137a6
a0ki-github <aoki.hotaka@gmail.com>
2021-11-07 16:40:05 +0900

    Add: Userアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
-  namespace :admin do    
+  namespace :admin do
     resources :candidates
+  end
+  namespace :api, format: 'json' do
+    resources :users, only: %i(create update) do
+      get 'me', on: :collection
+    end
   end
   get '*path', to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
