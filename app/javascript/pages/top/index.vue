7ceea423399ab32e5f2c136cf5ba6adf0b86d1ef
a0ki-github <aoki.hotaka@gmail.com>
2021-11-10 10:59:45 +0900

    Add: トップページにBET画面へのリンクを作成

--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -6,13 +6,28 @@
       li BETするメンバーは変更可能だが、 <br> 番組開始(11/25 22:00)後は1回につき-10P
       li 各放送日時点で脱落メンバーにBETしているユーザー、または誰にもBETしていないユーザーには-20P
       li 番組終了時に得点上位者を発表
-    .text-center
+    .text-center(v-if="authUser")
       router-link(
+        :to="{ name: 'BettingIndex' }"
         class="btn btn-dark m-3 router-link-active"
+      ) BET
+    .text-center(v-else)
+      router-link(
         :to="{ name: 'LoginIndex' }"
+        class="btn btn-dark m-3 router-link-active"
       ) ログイン
       router-link(
-        class="btn btn-dark m-3 router-link-active"
         :to="{ name: 'RegisterIndex' }"
+        class="btn btn-dark m-3 router-link-active"
       ) ユーザー登録
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'TopIndex',
+  computed: {
+    ...mapGetters(['authUser'])
+  }
+}
+</script>
