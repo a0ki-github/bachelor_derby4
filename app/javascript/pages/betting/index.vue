682072b2cdc34420894aec0ba9d996b79c06b27c
a0ki-github <aoki.hotaka@gmail.com>
2021-11-10 17:31:46 +0900

    Add: BETフォームを作成

--- app/javascript/pages/betting/index.vue
+++ app/javascript/pages/betting/index.vue
@@ -1,9 +1,54 @@
 <template lang="pug">
-  p hello
+  #betting-form.container.w-75
+    .h3.mb-3.text-center BET
+    ValidationObserver(v-slot="{ handleSubmit }")
+      .form-group
+        ValidationProvider(
+            v-slot="{ errors }"
+            rules="required"
+          )
+          p.text-danger.my-3 {{ errors[0] }}
+          template(v-for="candidate in candidates")
+            input(
+              type="radio"
+              name="候補者"
+              :id="candidate.id"
+              :value="candidate.id"
+              v-model="betting.candidate_id"
+            )
+            label(:for="candidate.id") {{ candidate.name }}({{ candidate.age }}) {{ candidate.title }}
+            br
+          p.text-danger.my-3 {{ errors[0] }}
+      button(
+        type="submit"
+        class="btn btn-primary my-2 d-block mx-auto"
+        @click.prevent="handleSubmit(createBetting)"
+      ) BETする
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
-  name: 'BettingIndex'
+  name: 'BettingIndex',
+  data() {
+    return {
+      betting: {
+        candidate_id: ''
+      }
+    }
+  },
+  created() {
+    this.fetchCandidates();
+  },
+  computed: {
+    ...mapGetters('candidates', ['candidates'])
+  },
+  methods: {
+    ...mapActions('candidates', ['fetchCandidates']),
+    async createBetting() {
+      const res = await this.$axios.post('bettings', { betting: this.betting })
+      this.$router.push({ name: 'TopIndex' })
+    }
+  }
 }
 </script>
