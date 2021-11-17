a9023673305863583842c5aa71ee0220fa1d1d14
a0ki-github <aoki.hotaka@gmail.com>
2021-11-17 09:59:49 +0900

    Fix: BET一覧画面でのフォーム形式とYoutube埋め込みの廃止。サムネイルの導入。

--- app/javascript/pages/betting/index.vue
+++ app/javascript/pages/betting/index.vue
@@ -3,46 +3,16 @@
     .h1.mb-5.text-center BET
     .h5.mb-5.text-center(v-if="authUser.current_candidate")
       | 現在のBET
-      h3 {{ summary(authUser.current_candidate) }}
-    ValidationObserver(v-slot="{ handleSubmit }")
-      .form-group
-        ValidationProvider(
-            v-slot="{ errors }"
-            rules="required"
-          )
-          p.text-danger.my-3 {{ errors[0] }}
-          div(
-            v-for="candidate in except_current_candidates"
-            class="mb-5 text-center"
-          )
-            input(
-              type="radio"
-              name="候補者"
-              :id="candidate.id"
-              :value="candidate.id"
-              v-model="betting.candidate_id"
-            )
-            label(
-              :for="candidate.id"
-              class="h5"
-            )
-              | {{ candidate.name }}({{ candidate.age }})
-              br
-              | {{ candidate.title}}
-            br
-            TheYoutube(:youtube_url="candidate.youtube_url")
-            br
-          p.text-danger.my-3 {{ errors[0] }}
-      .text-center
-        button(
-          type="submit"
-          class="btn btn-primary my-2 d-block mx-auto"
-          @click.prevent="handleSubmit(handleCreateBetting)"
-        ) BETする
-        router-link(
-          :to="{name: 'TopIndex'}"
-          class="btn btn-secondary m-2"
-        ) 戻る
+      h3.mb-5 {{ summary(authUser.current_candidate) }}
+      div(
+        v-for="candidate in except_current_candidates"
+        @click="handleShowBettingCreateModal"
+      )
+        h5.mt-5 {{ summary(candidate) }}
+        img(
+          :src="`//i.ytimg.com/vi/${identifier(candidate.youtube_url)}/mqdefault.jpg`"
+          width="100%"
+        )
 </template>
 
 <script>
@@ -55,14 +25,15 @@ export default {
     return {
       betting: {
         candidate_id: ''
-      }
+      },
+      isVisibleBettingCreateModal: false
     }
   },
   created() {
     this.fetchCandidates();
   },
   computed: {
-    ...mapGetters('candidates', ['candidates', 'summary']),
+    ...mapGetters('candidates', ['candidates', 'summary', 'identifier']),
     ...mapGetters('users', ['authUser']),
     except_current_candidates() {
       if (this.authUser.current_candidate) {
@@ -75,6 +46,9 @@ export default {
   methods: {
     ...mapActions('users', ['createBetting']),
     ...mapActions('candidates', ['fetchCandidates']),
+    handleShowBettingCreateModal() {
+      this.isVisibleBettingCreateModal = true
+    },
     async handleCreateBetting() {
       if (new Date > new Date(2021, 10, 25, 22)) {
         if (window.confirm('-10Pとなりますが、よろしいですか？')) {
