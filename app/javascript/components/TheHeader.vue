b5de8104daa739c851b8d07b5a92f5ff32f9a2ad
a0ki-github <aoki.hotaka@gmail.com>
2021-11-18 09:25:13 +0900

    Fix: ヘッダータイトルを英字化しサイズUP

--- app/javascript/components/TheHeader.vue
+++ app/javascript/components/TheHeader.vue
@@ -3,8 +3,8 @@
     nav.navbar.navbar-expand.navbar-dark.bg-dark.justify-content-between
       router-link(
         :to="{name: 'TopIndex'}"
-        class="navbar-brand mb-0 h1"
-      ) バチェラーダービー4
+        class="navbar-brand mx-3 fs-3 fw-bold"
+      ) BachelorDerby4
       ul(
         v-if="authUser"
         class="navbar-nav"
