9ac27f6d350fb60e3454f24e24880e9109c78f8c
a0ki-github <aoki.hotaka@gmail.com>
2021-11-18 21:42:15 +0900

    Add: JWTトークンの鍵としてcredentialsを追加

--- app/controllers/concerns/api/user_authenticator.rb
+++ app/controllers/concerns/api/user_authenticator.rb
@@ -3,7 +3,7 @@ module Api::UserAuthenticator
     return @current_user if @current_user
     return unless bearer_token
 
-    decoded_token = JWT.decode bearer_token, nil, false
+    decoded_token = JWT.decode bearer_token, Rails.application.credentials.secret_key_base, false
     @current_user = User.find(decoded_token[0]['user_id'])
   end
 
--- app/models/concerns/jwt_token.rb
+++ app/models/concerns/jwt_token.rb
@@ -3,6 +3,6 @@ module JwtToken
     exp = Time.current + 1.month
     payload = { user_id: id, exp: exp.to_i }
 
-    JWT.encode payload, nil, 'none'
+    JWT.encode payload, Rails.application.credentials.secret_key_base, 'none'
   end
 end
