3a6067876f8221d27ce71a4800fdd8a43f85fe74
a0ki-github <aoki.hotaka@gmail.com>
2021-11-18 20:23:55 +0900

    Add: 管理者ページのログイン制御を実装

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,9 @@
 class Admin::BaseController < ApplicationController
+  before_action :require_admin
+
+  private
+
+  def require_admin
+    redirect_to admin_login_path unless User.find_by(id: session[:admin_user_id])&.admin?
+  end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,14 +1,21 @@
 class Admin::SessionsController < Admin::BaseController
+  skip_before_action :require_admin
+
   def new; end
 
   def create
     @user = login(params[:session][:name], params[:session][:password])
 
-    debugger
     if @user && @user.admin?
+      session[:admin_user_id] = @user.id
       redirect_to admin_episodes_path, success: '管理者ログインに成功'
     else
-      redirect_to root_path
+      render :new
     end
+  end
+
+  def destroy
+    logout
+    redirect_to admin_login_path
   end
 end
