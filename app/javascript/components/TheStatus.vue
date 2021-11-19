b1ac9b8d822f3b52b7f84a2d4169f2b38ba837d7
a0ki-github <aoki.hotaka@gmail.com>
2021-11-19 23:17:14 +0900

    Modify: 成績表示を見やすく修正

--- app/javascript/components/TheStatus.vue
+++ app/javascript/components/TheStatus.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
-  #status.text-center.my-5.text-white
+  #status.text-center.my-5
     h1.text-center.fw-bold.text-white 成績
     template(v-if="authUser.current_candidate")
-      .d-inline-block.m-4 現在のポイント
-        h3 {{ authUser.points }}P
-      .d-inline-block.m-4 現在のBET
-        h3 {{ authUser.current_candidate.name }}
+      .row.row-cols-3
+        .card.my-5.m-auto
+          .card-body
+            .card-title 現在のポイント
+            h1.card-text.fw-bold {{ authUser.points }}P
+      .text-white
+        span 現在のBET
+        h3.fw-bold
+          | {{ authUser.current_candidate.name }} ({{authUser.current_candidate.age}})
+          br
+          | {{ authUser.current_candidate.title }}
       TheYoutube(:youtube_url="authUser.current_candidate.youtube_url")
     template(v-else)
       span まだBETはありません。
