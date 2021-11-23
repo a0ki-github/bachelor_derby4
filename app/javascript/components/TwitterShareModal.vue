9e61918c4ba9bc65c0c8c8ee355a42698beb2458
a0ki-github <aoki.hotaka@gmail.com>
2021-11-23 11:58:03 +0900

    Add: BET済のユーザーにTwitterシェアのお願いモーダルを表示

---
+++ app/javascript/components/TwitterShareModal.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  transition(name="fade")
+    #contact
+      .modal(@click.self="handleCloseModal")
+        .modal-dialog
+          .modal-content
+            .modal-header
+              h4.modal-title Twitterシェア
+            .modal-body
+              p.fw-bold あなたのBETをTwitterでシェアすると開発者からめちゃくちゃ感謝されます。
+              TheTwitterShareButton
+              img.w-100(src="~pray.svg")
+            .modal-footer
+              button(
+                type="button"
+                class="btn btn-secondary"
+                @click="handleCloseModal"
+              ) シェアしない
+      .modal-backdrop.show
+</template>
+
+<script>
+import 'pray.svg'
+import TheTwitterShareButton from './TheTwitterShareButton.vue'
+export default {
+  name: '',
+  components: { TheTwitterShareButton },
+  methods: {
+    handleCloseModal() {
+      this.$emit('close-modal')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .modal {
+    display: block;
+  }
+</style>
---
+++ app/javascript/components/TwitterShareProposal.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  transition(name="fade")
+    TwitterShareModal(
+      v-if="isVisibleTwitterShareModal && authUser.current_candidate"
+      @close-modal="handleCloseTwitterShareModal"
+    )
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import TwitterShareModal from './TwitterShareModal.vue'
+export default {
+  name: 'TwitterShareProposal',
+  components: { TwitterShareModal },
+  data() {
+    return {
+      isVisibleTwitterShareModal: true
+    }
+  },
+  computed: {
+    ...mapGetters('users', ['authUser'])
+  },
+  methods: {
+    handleCloseTwitterShareModal() {
+      this.isVisibleTwitterShareModal = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
